63d6368354ede23de4b4d598e63b7db66f0efcfe
Yoyo <yoyo@example.com>
2019-07-04 15:21:32 +0800

    1

--- num.php
+++ num.php
@@ -42,15 +42,22 @@ function go(){
     $num = 10000; //总次数
     $offSet = 4; //偏移量
     $z = 0;
+
+    $start = microtime(true);
+
     for ($i=0; $i < $num; $i ++){
         if(prize($pro))
             $z++;
     }
 
-    echo $num . "\n";
-    echo $z . "\n";
+    $end = microtime(true);
 
-    echo "######################\n";
+    NumStack::$allTimePeriod[] = $end-$start;
+    NumStack::$allPoint[] = $z;
+//    echo $num . "\n";
+//    echo $z . "\n";
+//
+//    echo "######################\n";
 
     /*
     比如预设的中奖率是 10% ,   然后实际发出1000个  中奖150个 =  15%  就把当前的中奖率往下调调
@@ -59,6 +66,7 @@ function go(){
     = 本轮实际中奖率是5%
 
      */
+    $start = microtime(true);
     $autoZ = 0;
     for ($i=0; $i < $num; $i ++){
         $tPro = $pro - (max(($autoZ / $num * 100) + $offSet, $pro) - $pro);
@@ -67,15 +75,64 @@ function go(){
         if(prize($tPro))
             $autoZ++;
     }
-    echo $num . "\n";
-    echo $autoZ . "\n";
-
-    echo "End######################\n";
-    echo "\n";
-    echo "\n";
+    $end = microtime(true);
+    NumStack::$allTimeAPeriod[] = $end-$start;
+    NumStack::$allAPoint[] = $autoZ;
+//    echo $num . "\n";
+//    echo $autoZ . "\n";
+//
+//    echo "End######################\n";
+//    echo "\n";
+//    echo "\n";
 }
 
 
-for ($i=0; $i < 10; $i ++){
+class NumStack{
+
+    static  $startTime = 0;
+    static  $endTime = 0;
+
+
+    static $allTimePeriod = [];
+    static $allTimeAPeriod = [];
+    static $allPoint = [];  //所有中奖数
+    static $allAPoint = [];  //所有中奖数
+}
+
+
+NumStack::$startTime = microtime(true);
+for ($i=0; $i < 100; $i ++){
     go();
 }
+
+NumStack::$endTime = microtime(true);
+
+
+sort(NumStack::$allTimePeriod);
+sort(NumStack::$allPoint);
+sort(NumStack::$allTimeAPeriod);
+sort(NumStack::$allAPoint);
+
+echo "Rand:\n";
+echo "min time: ".NumStack::$allTimePeriod[0]."\n";
+echo "max time:". NumStack::$allTimePeriod[count(NumStack::$allTimePeriod) -1] ."\n";
+echo "average time: " . (array_sum(NumStack::$allTimePeriod) / count(NumStack::$allTimePeriod)) . "\n";
+echo "min winner: ".NumStack::$allPoint[0]."\n";
+echo "max winner:".NumStack::$allPoint[count(NumStack::$allPoint)-1]."\n";
+echo "average winner:".(array_sum(NumStack::$allPoint) / count(NumStack::$allPoint))."\n";
+echo "######################\n";
+echo "Adjust Rand:\n";
+echo "min time: ".NumStack::$allTimeAPeriod[0]."\n";
+echo "max time:". NumStack::$allTimeAPeriod[count(NumStack::$allTimeAPeriod) -1] ."\n";
+echo "average time: " . (array_sum(NumStack::$allTimeAPeriod) / count(NumStack::$allTimeAPeriod)) . "\n";
+echo "min winner: ".NumStack::$allAPoint[0]."\n";
+echo "max winner:".NumStack::$allAPoint[count(NumStack::$allAPoint)-1]."\n";
+echo "average winner:".(array_sum(NumStack::$allAPoint) / count(NumStack::$allAPoint))."\n";
+
+
+echo 'startTime:' . NumStack::$startTime;
+echo "\n";
+echo 'EndTime:' . NumStack::$endTime;
+echo "\n";
+echo 'Consuming:' . (NumStack::$endTime - NumStack::$startTime);
+echo "\n";
